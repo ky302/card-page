9592300a57ae404d20c2290770cd255d17ceacca
ahmadsubekhi <ahmadsubekhi550@gmail.com>
2022-09-06 14:34:26 +0800

    update api

---
+++ index.js
@@ -0,0 +1,11 @@
+const fetchAdvice = async () => {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+
+  document.querySelector(".advice-id").innerHTML = data.slip.id;
+  document.querySelector(".advice").innerHTML = data.slip.advice;
+};
+
+fetchAdvice();
+
+document.querySelector(".btn").addEventListener("click", fetchAdvice);
